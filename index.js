18e21724559dfa6ebd4b44557241c55cffbe263f
abbo <thomas.abbondi@opiquad.it>
2024-04-30 08:46:50 +0200

    feat: add url as cli args

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { parseArgs } from "node:util";
 
 const MAIN_DOMAIN = "https://golarion.altervista.org";
 
@@ -6,15 +7,36 @@ axios.defaults.baseURL = MAIN_DOMAIN;
 axios.defaults.headers["User-Agent"] =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36";
 
-async function performScraping() {
+/**
+ * The arguments accepted by the CLI
+ */
+const options = {
+  url: {
+    type: "string",
+    default: "/wiki/Pagina_principale",
+  },
+};
+
+/**
+ * The parsed arguments
+ */
+const {
+  values: { url },
+} = parseArgs({ options });
+
+/**
+ *
+ */
+async function performScraping(url = "") {
   // downloading the target web page
   // by performing an HTTP GET request in Axios
   const axiosResponse = await axios.request({
     method: "GET",
-    url: "/wiki/Pagina_principale",
+    url,
   });
-
-  console.debug(axiosResponse);
 }
 
-performScraping();
+/**
+ * Main process
+ */
+performScraping(url);
